9d9a84b152e945d5d0f0b11cebbd35f330857fea
Rissa Waters <rissa3896@yahoo.com>
2014-10-06 10:40:37 -0500

    Made a dice game in larval, the user can guess in the url and the page will output whether the user guessed the roll right or wrong.

---
+++ app/views/roll-dice.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+<style>
+body {
+	background-color: white;
+}
+</style>
+	<h1>Roll the dice!</h1>
+	<h3>Your guess: <?= $guess ?></h3>
+	<h3>Your roll: <?= $random ?></h3>
+	<?php if ($guess == $random): ?>
+		<h2>You Win!</h2>
+	<?php else : ?>
+		<h2>You Lose!</h2>
+	<?php endif ?>
+@stop
