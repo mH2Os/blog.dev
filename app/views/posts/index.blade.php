6426be37875bf64146b7ee529d66521a7d96ceb2
Rissa Waters <rissa3896@yahoo.com>
2014-10-09 13:31:36 -0500

    Added a delete button to my blog.

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -12,8 +12,12 @@
 	<h4><a href="{{{ action('PostsController@show', $post->id) }}}" id="a_title">{{{ $post->title }}}</a></h4>
 	<h4>{{{ $post->body }}}</h4>
 	<p>{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}} </p>
+
+	<p>{{ Form::open(['method' => 'DELETE', 'action' => ['PostsController@destroy', $post->id]])}}
+		<button type="submit" class="btn btn-link">Delete</button>
+	{{ Form::close() }}	<a href="{{{ action('PostsController@edit', $post->id) }}}">Edit</a></p>
+
 	<br>
-	<a href="{{{ action('PostsController@edit', $post->id) }}}">Edit</a>
 	<br>
 
 @endforeach
