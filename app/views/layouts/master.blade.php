37292e82faebea524d6bc73010a4869d09413091
Rissa Waters <rissa3896@yahoo.com>
2014-10-10 09:08:34 -0500

    Edited my master blade navbar.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -31,7 +31,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand active" id="start" href="{{{ action('PostsController@index') }}}">Home</a>
+          <a class="navbar-brand active" id="start" href="{{{ action('PostsController@index') }}}">Rissa's World</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
